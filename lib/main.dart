a919b9588f29b08a41cc3438aa1b4d3b22e18b08
Ibrar Khan <ibrarkhan431414@gmail.com>
2025-01-24 22:56:25 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:service_app/View%20Model/Auth_Provider/auth_provider.dart';
 import 'package:service_app/View%20Model/Auth_Provider/signup_provider.dart';
 import 'package:service_app/View/Get_Started/get_started_screen.dart';
+import 'package:service_app/firebase_options.dart';
 
 
-void main(){
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp(
+     options: DefaultFirebaseOptions.currentPlatform
+   );
   runApp(
     MultiProvider(
         providers: [
